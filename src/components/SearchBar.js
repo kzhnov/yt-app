990b3dcb5dd0ee97c21961a8391a4c73dbff8b2e
Zin Hein <zin.hein@bza.co>
2021-05-22 14:14:26 +0800

    update label

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -13,7 +13,7 @@ const SearchBar = ({ onFormSubmit }) => {
     <div className="search-bar ui segment">
       <form onSubmit={onSubmit} className="ui form">
         <div className="field">
-          <label>Vidoe Search</label>
+          <label>Search for a Video</label>
           <input
             type="text"
             onChange={e => setTerm(e.target.value)}
